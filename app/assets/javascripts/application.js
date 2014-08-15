e0dbecdee878cfa7e5547f3dd77081d12bf8054a
Layne McNish <lmcnish14@gmail.com>
2014-08-15 11:32:12 -0600

    switched to google maps api

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+  function initialize() {
+    var mapOptions = {
+      center: new google.maps.LatLng(40.7542789, -73.9765479),
+      zoom: 13
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"),
+      mapOptions);
+  }
+
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+
+});
