e3f14609c3a89a295f21450d41a5ed735ad339d2
Layne McNish <lmcnish14@gmail.com>
2014-08-18 17:00:38 -0600

    changed from drop down to input boxes for address search. directions now on right and working when the addresses are valid.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,93 +14,48 @@
 //= require jquery_ujs
 //= require_tree .
 
-//$(document).ready(function () {
-//  var map;
-//  var directionsDisplay;
-//  var directionsService;
-//  var stepDisplay;
-//  var markerArray = [];
-//
-//  function initialize() {
-//    // Instantiate a directions service.
-//    directionsService = new google.maps.DirectionsService();
-//
-//    // Create a map and center it on Manhattan.
-//    var manhattan = new google.maps.LatLng(40.7711329, -73.9741874);
-//    var mapOptions = {
-//      zoom: 13,
-//      center: manhattan
-//    }
-//    map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-//
-//    // Create a renderer for directions and bind it to the map.
-//    var rendererOptions = {
-//      map: map
-//    }
-//    directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions)
-//
-//    // Instantiate an info window to hold step text.
-//    stepDisplay = new google.maps.InfoWindow();
-//  }
-//
-//  function calcRoute() {
-//
-//    // First, remove any existing markers from the map.
-//    for (var i = 0; i < markerArray.length; i++) {
-//      markerArray[i].setMap(null);
-//    }
-//
-//    // Now, clear the array itself.
-//    markerArray = [];
-//
-//    // Retrieve the start and end locations and create
-//    // a DirectionsRequest using WALKING directions.
-//    var start = document.getElementById('start').value;
-//    var end = document.getElementById('end').value;
-//    var request = {
-//      origin: start,
-//      destination: end,
-//      travelMode: google.maps.TravelMode.WALKING
-//    };
-//
-//    // Route the directions and pass the response to a
-//    // function to create markers for each step.
-//    directionsService.route(request, function(response, status) {
-//      if (status == google.maps.DirectionsStatus.OK) {
-//        var warnings = document.getElementById('warnings_panel');
-//        warnings.innerHTML = '<b>' + response.routes[0].warnings + '</b>';
-//        directionsDisplay.setDirections(response);
-//        showSteps(response);
-//      }
-//    });
-//  }
-//
-//  function showSteps(directionResult) {
-//    // For each step, place a marker, and add the text to the marker's
-//    // info window. Also attach the marker to an array so we
-//    // can keep track of it and remove it when calculating new
-//    // routes.
-//    var myRoute = directionResult.routes[0].legs[0];
-//
-//    for (var i = 0; i < myRoute.steps.length; i++) {
-//      var marker = new google.maps.Marker({
-//        position: myRoute.steps[i].start_location,
-//        map: map
-//      });
-//      attachInstructionText(marker, myRoute.steps[i].instructions);
-//      markerArray[i] = marker;
-//    }
-//  }
-//
-//  function attachInstructionText(marker, text) {
-//    google.maps.event.addListener(marker, 'click', function() {
-//      // Open an info window when the marker is clicked on,
-//      // containing the text of the step.
-//      stepDisplay.setContent(text);
-//      stepDisplay.open(map, marker);
-//    });
-//  }
-//
-//  google.maps.event.addDomListener(window, 'load', initialize);
+var directionsDisplay;
+var directionsService = new google.maps.DirectionsService();
 
-//});
+function initialize() {
+  directionsDisplay = new google.maps.DirectionsRenderer();
+  var mapOptions = {
+    zoom: 12,
+    center: new google.maps.LatLng(40.782865, -73.965355)
+  };
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+    mapOptions);
+  directionsDisplay.setMap(map);
+  directionsDisplay.setPanel(document.getElementById('directions-panel'));
+
+  var control = document.getElementById('control');
+  control.style.display = 'block';
+  map.controls.push(control);
+
+  $("#control input[type=submit]").on('click', calcRoute);
+}
+
+function calcRoute(event) {
+  event.preventDefault();
+  console.log("hello");
+  var start = document.getElementById('start').value;
+  var end = document.getElementById('end').value;
+  console.log(start);
+  console.log(end);
+  var request = {
+    origin: start,
+    destination: end,
+    travelMode: google.maps.TravelMode.WALKING
+
+  };
+
+  directionsService.route(request, function (response, status) {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsDisplay.setDirections(response);
+      var contentRight = document.getElementById('content-right');
+      contentRight.style.display = 'block';
+      contentRight.style.background = '#f4f4f4';
+    }
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
