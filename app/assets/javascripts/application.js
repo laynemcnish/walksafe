0945b97e86bef0e6edbe4a8d4b05fcb59c513b59
Layne McNish <lmcnish14@gmail.com>
2014-09-05 13:04:52 -0600

    added draggable directions functionality

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,29 +2,20 @@
 //= require jquery_ujs
 //= require_tree .
 
-var rendererOptions = {
-  draggable: true
-};
-var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
+var directionsDisplay = new google.maps.DirectionsRenderer({ draggable: true });
 var directionsService = new google.maps.DirectionsService();
-var rboxer = new RouteBoxer();
-var distance = 0.4; // km
 var map;
 
 function initialize() {
-  directionsDisplay = new google.maps.DirectionsRenderer();
   var mapOptions = {
     zoom: 13,
-    center: new google.maps.LatLng(39.7386033, -104.935449)
+    center: new google.maps.LatLng(39.7386033, -104.935449),
+    draggable: true
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
     mapOptions);
   directionsDisplay.setMap(map);
   directionsDisplay.setPanel(document.getElementById('directions-panel'));
-
-  google.maps.event.addListener(directionsDisplay, 'directions_changed', function () {
-    computeTotalDistance(directionsDisplay.getDirections());
-  });
 
   cartodb.createLayer(map, 'http://lmcnish14.cartodb.com/api/v2/viz/ba1f60ea-2fac-11e4-b64f-0e73339ffa50/viz.json')
     .addTo(map)
@@ -33,11 +24,9 @@ function initialize() {
       sublayer.on('featureOver', function (e, pos, latlng, data) {
         cartodb.log.log(e, pos, latlng, data);
       });
-
       sublayer.on('error', function (err) {
         cartodb.log.log('error: ' + err);
       });
-
     })
     .on('error', function () {
       cartodb.log.log("some error occurred");
@@ -69,18 +58,13 @@ function calcRoute(event) {
   directionsService.route(request, function (response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
       directionsDisplay.setDirections(response);
-//      var path = result.routes[0].overview_path;
-//      var boxes = routeBoxer.box(path, distance);
-//
-//      for (var i = 0; i < boxes.length; i++) {
-//        var bounds = box[i];}
-
       var contentRight = document.getElementById('directions-panel');
       contentRight.style.display = 'block';
       contentRight.style.background = '#f4f4f4';
     }
   });
 }
+
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
