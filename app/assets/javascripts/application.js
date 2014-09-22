742e58ab3c748ec76896fa575c0b83cc41b92edb
Layne McNish <lmcnish14@gmail.com>
2014-09-22 16:53:24 -0600

    resolved addListener issue by moving it directly after markers.push(marker)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,7 +5,6 @@
 var rendererOptions = {draggable: false};
 var directionsDisplay = new google.maps.DirectionsRenderer(rendererOptions);
 var directionsService = new google.maps.DirectionsService();
-var infowindow = new google.maps.InfoWindow();
 var map;
 var distance = 0.1; // km
 var boxpolys = [];
@@ -22,13 +21,14 @@ function initialize() {
   directionsDisplay.setPanel(document.getElementById('directions-panel'));
 
 //  ***************CRIME POINT MAP LAYER ************************
-//  cartodb.createLayer(map, 'http://lmcnish14.cartodb.com/api/v2/viz/ba1f60ea-2fac-11e4-b64f-0e73339ffa50/viz.json')
-//    .addTo(map);
+  cartodb.createLayer(map, 'http://lmcnish14.cartodb.com/api/v2/viz/ba1f60ea-2fac-11e4-b64f-0e73339ffa50/viz.json')
+    .addTo(map);
 //  ************************************************************
 
   var control = document.getElementById('control');
   control.style.display = 'block';
   map.controls.push(control);
+
 
   $("#control input[type=submit]").on('click', calcRoute);
 
@@ -52,7 +52,6 @@ function calcRoute(event) {
 
   directionsService.route(request, function (response, status) {
     if (status == google.maps.DirectionsStatus.OK) {
-      console.log("route called");
       directionsDisplay.setDirections(response);
       var contentRight = document.getElementById('directions-panel');
       contentRight.style.display = 'block';
@@ -62,7 +61,6 @@ function calcRoute(event) {
       var routeBoxer = new RouteBoxer();
       var boxes = routeBoxer.box(path, distance);
 
-      console.log("after route called:", path);
       drawBoxes(boxes);
       google.maps.event.addListener(directionsDisplay, 'routeindex_changed', function () {
         var current_route_index = this.getRouteIndex();
@@ -77,8 +75,6 @@ function calcRoute(event) {
 }
 
 function drawBoxes(boxes) {
-  console.log("drawboxes activated");
-  console.log("boxes", boxes);
   var count = 0;
   var crime_count = 0;
   var promise = $.getJSON("http://lmcnish14.cartodb.com/api/v2/sql?q=SELECT geo_lon, geo_lat, severity FROM public.crime_updated");
@@ -115,11 +111,11 @@ function drawBoxes(boxes) {
       service3.search(place_request3, callback);
       boxpolys[i] = new google.maps.Rectangle({
 //       ************* BOX BORDERS ********************************************
-//        bounds: boxes[i],
-//        fillOpacity: 0,
-//        strokeOpacity: 1.0,
-//        strokeColor: '#000000',
-//        strokeWeight: 1,
+        bounds: boxes[i],
+        fillOpacity: 0,
+        strokeOpacity: 1.0,
+        strokeColor: '#000000',
+        strokeWeight: 1,
 //        *********************************************************************
         map: map
       });
@@ -138,12 +134,10 @@ function drawBoxes(boxes) {
     }
     var avg_crime = parseInt(count / crime_count);
     $('#severity_score').replaceWith('<div id="severity_score"><p><strong>Number of Crimes: ' + crime_count + '  |   Avg Score: ' + avg_crime + '  |  Total Score: ' + count + '</strong></p></div>');
-    console.log(count);
   });
 }
 
 function clearBoxes() {
-  console.log("clearBoxes activated");
   for (var i = 0; i < boxpolys.length; i++) {
     boxpolys[i].setMap(null);
 
@@ -162,34 +156,37 @@ function callback(results, status) {
       var request = { reference: results[i].reference };
       service.getDetails(request, function (details, status2) {
         //if(status2 == google.maps.places.PlacesServiceStatus.OK)
-        addResult(details);
+//        addResult(details);
       });
       i++;
     }
   }
 }
 
-function addResult(place) {
-  if (place != null) {
-
-    // You should now have a "place" object here that has address, name, URL, etc...
-  }
-}
+//function addResult(place) {
+//  if (place != null) {
+//
+//    // You should now have a "place" object here that has address, name, URL, etc...
+//  }
+//}
 
 function createMarker(place) {
   var str = place.name;
-  if (str.indexOf("Green") == -1 && str.indexOf("Medicinals") == -1 && str.indexOf("Herban") == -1 && str.indexOf("Dispensary") == -1 && str.indexOf("Paternity") == -1 && str.indexOf("Mining") == -1 && str.indexOf("Plastic") == -1 && str.indexOf("Dentist") == -1 && str.indexOf("Dermatology") == -1 && str.indexOf("Marijuana") == -1)
-  {
+  if (str.indexOf("Green") == -1 && str.indexOf("Medicinals") == -1 && str.indexOf("Herban") == -1 && str.indexOf("Dispensary") == -1 && str.indexOf("Paternity") == -1 && str.indexOf("Mining") == -1 && str.indexOf("Plastic") == -1 && str.indexOf("Dentist") == -1 && str.indexOf("Dermatology") == -1 && str.indexOf("Marijuana") == -1) {
     var marker = new google.maps.Marker({
       map: map,
-      position: place.geometry.location
+      position: place.geometry.location,
+      clickable: true
 
     });
+    marker.info = new google.maps.InfoWindow({
+      content: '<p>' + place.name + '</p>' + '<p>' + place.vicinity + '</p>'
+    });
     markers.push(marker);
-    console.log(place.name);
+    google.maps.event.addListener(marker, 'click', function () {
+      marker.info.open(map, marker);
+    });
   }
-
-
 
 
 }
@@ -202,10 +199,6 @@ function deleteMarkers() {
 
   markers = [];
 }
-
-
-
-console.log('Boxpolys:', boxpolys);
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
